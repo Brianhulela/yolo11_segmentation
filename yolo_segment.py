002d6e6a46598578118c442a8c6f14fe0931ca53
Brianhulela <101281239+Brianhulela@users.noreply.github.com>
2024-10-01 23:23:52 +0200

    Load and run the YOLO11 instance segmentation result

--- yolo_segment.py
+++ yolo_segment.py
@@ -1,4 +1,6 @@
+from ultralytics import YOLO
 import cv2
+import numpy as np
 import urllib.request
 
 url, filename = ("https://images.unsplash.com/photo-1484353371297-d8cfd2895020?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTUwfHxwZW9wbGV8ZW58MHx8MHx8fDA%3D", "scene.jpg")
@@ -7,4 +9,11 @@ urllib.request.urlretrieve(url, filename)    # Download the image
 # Load the input image using OpenCV
 image = cv2.imread("scene.jpg")
 
+# Load the model
+model = YOLO("yolo11n-seg.pt")  # load an official YOLO model
 
+# Predict with the model
+results = model("scene.jpg")  # predict on an image
+
+# Create an empty mask for segmentation
+segmentation_mask = np.zeros_like(image, dtype=np.uint8)
